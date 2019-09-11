27f21ee9c5559930569c0e4b8041777d723395f9
Reese Kunz <reesekunz21@gmail.com>
2019-09-11 13:53:24 -0700

    all crud operations working with helpers

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -4,30 +4,48 @@ const db = require("../data/db-config");
 function find() {
   return db("schemes");
 }
-// using findById(id) helper for GET to /:id in scheme router
+// using findById(id) helper for GET to /:id, POST to /:id/steps, and PUT to /:id in scheme router
 function findById(id) {
   return db("schemes").where({ id: id });
 }
 // using findSteps(id) helper for GET to /:id/steps in scheme router
 function findSteps(id) {
-  return (
-    db("schemes")
-      .join("steps", "steps.scheme_id", "=", "schemes.id")
-      .where({ scheme_id: id })
-      // .select(
-      //   "steps.id",
-      //   "schemes.scheme_name",
-      //   "steps.step_number",
-      //   "steps.instructions"
-      // )
-      .then(steps => {
-        return steps;
-      })
-  );
+  return db("schemes")
+    .join("steps", "steps.scheme_id", "=", "schemes.id")
+    .where({ scheme_id: id })
+    .select(
+      "steps.id",
+      "schemes.scheme_name",
+      "steps.step_number",
+      "steps.instructions"
+    )
+    .then(steps => {
+      return steps;
+    });
+}
+// using add(schme) helper for POST to / in scheme router
+function add(scheme) {
+  return db("schemes").insert(scheme);
+}
+
+// using add(scheme) helper for PUT to /:id in scheme router
+function update(changes, id) {
+  return db("schemes")
+    .where({ id: id })
+    .update(changes);
+}
+// using remove(id) helper for DELETE to /:id in scheme router
+function remove(id) {
+  return db("schemes")
+    .where({ id: id })
+    .del();
 }
 
 module.exports = {
   find,
   findById,
-  findSteps
+  findSteps,
+  add,
+  update,
+  remove
 };
