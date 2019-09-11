4905bcc2e621904329263d9c76fac78c7df37d95
Reese Kunz <reesekunz21@gmail.com>
2019-09-11 13:29:06 -0700

    added findById and findSteps helpers

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,10 +1,33 @@
 const db = require("../data/db-config");
 
+// using find () helper for GET to / in scheme router
 function find() {
-  // using find () helper for GET to / in scheme router
   return db("schemes");
+}
+// using findById(id) helper for GET to /:id in scheme router
+function findById(id) {
+  return db("schemes").where({ id: id });
+}
+// using findSteps(id) helper for GET to /:id/steps in scheme router
+function findSteps(id) {
+  return (
+    db("schemes")
+      .join("steps", "steps.scheme_id", "=", "schemes.id")
+      .where({ scheme_id: id })
+      // .select(
+      //   "steps.id",
+      //   "schemes.scheme_name",
+      //   "steps.step_number",
+      //   "steps.instructions"
+      // )
+      .then(steps => {
+        return steps;
+      })
+  );
 }
 
 module.exports = {
-  find
+  find,
+  findById,
+  findSteps
 };
